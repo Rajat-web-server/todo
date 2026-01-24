95275580c7f9307f8b149291daaca65d68d6756d
Your Name <rajatpandey542@gmail.com>
2026-01-24 20:55:44 +0530

    Day 1 : Review the code

--- todo/main.js
+++ todo/main.js
@@ -1,7 +1,11 @@
 const input = document.querySelector(".task");
-// localStorage.setItem("Tasks",input.value)
 const add = document.querySelector(".add");
 const show=document.querySelector(".showTask");
+const Delete = document.querySelector(".delete");
+const Update = document.querySelector(".update")
+
+//Here the thing I have created is creating and deleting functions
+
 add.addEventListener("click", () => {
     const show = document.querySelector(".showTask")
     // show.innerHTML=localStorage.getItem("value");
@@ -20,42 +24,46 @@ window.addEventListener("load",()=>{
 })
 
 
-const remove = document.querySelector(".delete");
-remove.addEventListener("click", () => {
+
+Delete.addEventListener("click", () => {
     const show = document.querySelector(".showTask")
     show.innerHTML = "";
 })
 
-const hours = document.getElementById("hour");
-for (let i=1;i<=12;i++){
-    const option = document.createElement("option");
-    option.value=i;
-    option.textContent=i;
-    hours.appendChild(option);
-}
+//How can I add the updating function :
 
-const minutes = document.getElementById("mins");
-for (let i=1;i<=60;i++){
-    const option = document.createElement("option");
-    option.value=i;
-    option.textContent=i;
-    minutes.appendChild(option);
-}
 
-const months = document.getElementById("month");
-for (let i=1;i<=12;i++){
-    const option = document.createElement("option");
-    option.value=i;
-    option.textContent=i;
-    months.appendChild(option);
-}
 
-const days = document.getElementById("day");
-for (let i=1;i<=31;i++){
-    const option = document.createElement("option");
-    option.value=i;
-    option.textContent=i;
-    days.appendChild(option);
-}
+// const hours = document.getElementById("hour");
+// for (let i=1;i<=12;i++){
+//     const option = document.createElement("option");
+//     option.value=i;
+//     option.textContent=i;
+//     hours.appendChild(option);
+// }
+
+// const minutes = document.getElementById("mins");
+// for (let i=1;i<=60;i++){
+//     const option = document.createElement("option");
+//     option.value=i;
+//     option.textContent=i;
+//     minutes.appendChild(option);
+// }
+
+// const months = document.getElementById("month");
+// for (let i=1;i<=12;i++){
+//     const option = document.createElement("option");
+//     option.value=i;
+//     option.textContent=i;
+//     months.appendChild(option);
+// }
+
+// const days = document.getElementById("day");
+// for (let i=1;i<=31;i++){
+//     const option = document.createElement("option");
+//     option.value=i;
+//     option.textContent=i;
+//     days.appendChild(option);
+// }
 
 
