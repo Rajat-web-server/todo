dafe2a7772167b3ba45e1d7b01f299d24f95f7da
Your Name <rajatpandey542@gmail.com>
2026-04-25 13:04:29 +0530

    Fix JSX conditional rendering using ternary

--- todo/To_do_list/src/components/Display.jsx
+++ todo/To_do_list/src/components/Display.jsx
@@ -1,10 +1,30 @@
-
-
+import { useState } from "react"
+import { Input } from "./input"
 export const Display =({Task})=>{
 
+    const [isUpdate, setisUpdate]=useState(false)
+    const [UpdatedTask,setisUpdatedTask]=useState(Task)
+
+    const Update =()=>{
+        setisUpdate(!isUpdate)
+    }
     return(
         <div>
-            <p>{Task}</p>
+            {
+            isUpdate ?(
+                <div>
+                <input type="text" value={Task} placeholder="Write your Task" onChange={(e)=>setisUpdatedTask(e.target.value)} />
+                </div>
+                
+    )
+            :(
+                <div>
+                <p>{Task}</p> 
+                </div>
+            )
+            }
+            
+            <button onClick={Update}>Edit</button>
         </div>
     )
 }
